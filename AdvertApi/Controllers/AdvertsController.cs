2be21caabf8325962032427f25e39c485722ae7b
Filipe Silva <filipeqs@outlook.com>
2022-09-27 11:44:46 -0400

    Initial changes

---
+++ AdvertApi/Controllers/AdvertsController.cs
@@ -0,0 +1,62 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AdvertsController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertsController(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(CreateAdvertResponse), StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            try
+            {
+                var recordId = await _advertStorageService.Add(model);
+
+                return StatusCode(StatusCodes.Status201Created, new CreateAdvertResponse { Id = recordId });
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+
+                return Ok();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
